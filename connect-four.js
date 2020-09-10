030d9c5843ad24bb5953fad14ee5600edd1d31ac
Ryan Webster <ryan.h.webster@gmail.com>
2020-09-10 16:06:33 -0700

    placing tokens

--- connect-four.js
+++ connect-four.js
@@ -7,23 +7,41 @@ let newGameBtn = document.getElementById("new-game");
 let board = document.getElementById("board-holder");
 
 function updateUI() {
-    console.log("hi");
     if (game === undefined) {
         board.classList.add("is-invisible");
     } else {
-        console.log("echo");
         board.classList.remove("is-invisible");
         document.getElementById("game-name").innerHTML = game.getName();
     }
+
+    for (let rowIndex = 0; rowIndex <= 5; rowIndex++) {
+        for(let columnIndex = 0; columnIndex <= 6; columnIndex++) {
+            let square = document.querySelector(`#square-${rowIndex}-${columnIndex}`)
+            square.innerHTML = '';
+            let playerNumber = game.getTokenAt(rowIndex, columnIndex);
+            let token = document.createElement('div');
+            if (playerNumber === 1) {
+                token.classList.add('token');
+                token.classList.add("red");
+                square.appendChild(token);
+            } else if (playerNumber === 2) {
+                token.classList.add('token');
+                token.classList.add("black");
+                square.appendChild(token);
+            }
+        }
+    }
     let currentPlayer = game.currentPlayer;
-    let clickTarget = document.getElementById("click-targets");
+    let clickTarget = document.getElementById('click-targets');
     if (currentPlayer === 1) {
         clickTarget.classList.add("red");
-        clickTarget.classList.remove("black");
-    } else {
+        clickTarget.classList.remove("black")
+        
+            } else {
         clickTarget.classList.add("black");
         clickTarget.classList.remove("red");
     }
+
 }
 
 window.document.addEventListener("DOMContentLoaded", (e) => {
@@ -56,9 +74,9 @@ window.document.addEventListener("DOMContentLoaded", (e) => {
         if (!targetId.startsWith("column-")) {
             return;
         } else {
-            let columnIndex = Number.parseInt(targetId);
+            let columnIndex = Number.parseInt(targetId[targetId.length-1]);
+            game.playInColumn(columnIndex);
+            updateUI();
         }
-        game.playInColumn();
-        updateUI();
     });
 });
