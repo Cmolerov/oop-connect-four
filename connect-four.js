d0393fc204f0c8349cd6a5d4196d41c33ef9b260
carlos molero <cmolero130@gmail.com>
2020-09-10 16:46:55 -0400

    define current player and click target color

--- connect-four.js
+++ connect-four.js
@@ -1,26 +1,26 @@
-import { Game } from './game.js';
-
+import { Game } from "./game.js";
 
 let game = undefined;
 let player1 = document.getElementById("player-1-name");
 let player2 = document.getElementById("player-2-name");
 let newGameBtn = document.getElementById("new-game");
-let board = document.getElementById('board-holder');
+let board = document.getElementById("board-holder");
 
 function updateUI() {
-    console.log('hi');
+    console.log("hi");
     if (game === undefined) {
-        board.classList.add('is-invisible');
+        board.classList.add("is-invisible");
     } else {
-        console.log('echo');
-        board.classList.remove('is-invisible');
+        console.log("echo");
+        board.classList.remove("is-invisible");
+        document.getElementById("game-name").innerHTML = game.getName();
     }
 }
 
 window.document.addEventListener("DOMContentLoaded", (e) => {
-
+    let currentPlayer;
     function enableNewGame() {
-        if (player1.value !== '' && player2.value !== '') {
+        if (player1.value !== "" && player2.value !== "") {
             newGameBtn.disabled = false;
         }
     }
@@ -33,12 +33,25 @@ window.document.addEventListener("DOMContentLoaded", (e) => {
     });
 
     newGameBtn.addEventListener("click", (e) => {
-        game = new Game();
-        player1.value = '';
-        player2.value = '';
+        game = new Game(player1.value, player2.value);
+        currentPlayer = game.currentPlayer;
+        player1.value = "";
+        player2.value = "";
+
         newGameBtn.disabled = true;
         updateUI();
     });
 
-    
+    let clickTarget = document.getElementById("click-targets");
+    clickTarget.addEventListener("click", (e) => {
+        game.playInColumn();
+        updateUI();
+        if (currentPlayer === 1) {
+            clickTarget.classList.add("red");
+            clickTarget.classList.remove("black");
+        } else {
+            clickTarget.classList.add("black");
+            clickTarget.classList.remove("red");
+        }
+    });
 });
--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 export class Game {
     constructor(player1name, player2name) {
-        this.player1 = player1name;
-        this.player2 = player2name;
+        this.player1name = player1name;
+        this.player2name = player2name;
+        this.currentPlayer = 1;
     }
 
     getName() {
-        document.getElementById('game-name').innerHTML = `${this.player1} vs ${this.player2}`
+        return `${this.player1name} vs ${this.player2name}`;
+    }
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
     }
 }
